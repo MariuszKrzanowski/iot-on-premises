cf942cf7bd91782b7b0a7e918d067d9d41af6814
Mariusz Krzanowsk <Mariusz.Krzanowski@gihub.com>
2020-11-28 11:29:59 +0100

    Comments added.

--- src/Arduino/SimpleSamples/01Serial/src/main.cpp
+++ src/Arduino/SimpleSamples/01Serial/src/main.cpp
@@ -1,21 +1,23 @@
 #include <Arduino.h>
 
-void setup()
+int counter = 0;
+
+// Runs ones after processor boot
+void setup()  
 {
   delay(50);
   Serial.begin(115200);
-  Serial.println(  );
-  Serial.println(  );
-  Serial.println(  );
+  Serial.println();
+  Serial.println();
   Serial.println("In setup");
 }
 
-int counter=0;
-
-void loop()
+// It called non stop time. 
+// NOTE!!! Do not block this loop.
+void loop()  
 {
-  delay(1000);
+  delay(1000); // This is system friendly method so it is safe.
   Serial.println("....");
-  Serial.printf("   %4d\r\n",counter);
-  counter=(++counter) % 100;
- }
+  Serial.printf("   %4d\r\n", counter);
+  counter = (++counter) % 100;
+}
